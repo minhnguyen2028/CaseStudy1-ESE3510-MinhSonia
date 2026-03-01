af697012b4a67a3a6b49940ac93f392c38a9c307
17mng <17mng@MinhsLaptop>
2026-02-28 23:53:29 -0600

    testing

---
+++ ESE3510_CS1.m
@@ -0,0 +1,11 @@
+%% ESE 3510 Signals and Systems - Case Study #1
+%%
+% * Authors: Joon Kim, Minh Duc Nguyen, Sonia Palamand 
+% * Class: ESE 3510-01
+% * Date: Started - 2/20/26 ; Completed - 2/XX/26
+% * Contributions from: ______
+
+%% Task 1: Initial Design of Multiband Filter
+
+
+%% Task 2: 
