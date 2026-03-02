d76db2e10e6beecb29f3840b9224250322e32886
17mng <17mng@MinhsLaptop>
2026-03-01 20:39:04 -0600

    (fixed) added helper functions

---
+++ audioEQ.m
@@ -0,0 +1,62 @@
+%% Helper function: audioEQ
+% Generates custom filter response w/ specified parameters 
+% INPUTS: 
+%   - fc: n-length vector of center frequencies to be used
+%   - Q: n-length vector of quality factors to be used (paired w/ fc)
+%   - gains: n-length vector of gains to be used 
+%   - debug: Boolean; 1 to print debug info; 0 for nothing
+%   - makePlot: Boolean; 1 to generate plots; 0 for nothing
+% OUTPUTS:
+%   - filters: n-length cell array of transfer functions specifying each band filter
+%   - H_total: transfer function generated from given parameters (response) 
+
+function [filters, H_total] = audioEQ(fc, Q, gains, debug, makePlot)
+    % Bandwidth
+    bw = fc./Q;
+    
+    if debug == 1
+        disp('Frequencies | Bandwidths: ')
+        for i = 1:length(fc)
+            disp([num2str(fc(i)), ' Hz | ', num2str(round(bw(i))), ' Hz'])
+        end
+    end
+
+    % Hz to rad/s conversion for CT transfer functions
+    w0 = 2 * pi * fc;   % resonant frequency (2pif)
+    beta = 2 * pi * bw; % bandwidth (R/L in series RLC circuit)
+        
+    % 2nd-order bandpass transfer function: H(s) = (beta * s) / (s^2 + beta * s + w0^2)
+    filters = cell(1, length(fc));
+    
+    for i = 1:length(fc)
+        num = [beta(i), 0];   %  tf() will see this as Beta(s) + 0
+        den = [1, beta(i), w0(i)^2];
+        filters{i} = tf(num, den); % builds continuous-time transfer function
+    end
+    
+    % "blank" transfer function. 0/1 = 0, but it's formatted as a transfer function object
+    H_total = tf(0, 1);
+    H_total_unity = tf(0, 1);
+    
+    % applies each gain, aggregates it to total system.
+    for i = 1:length(fc)
+        H_total = H_total + (gains(i) * filters{i});
+        H_total_unity = H_total_unity + (1 * filters{i});
+    end
+    
+    % Measure peak magnitude of the unity system across audible range
+    fNorm = logspace(log10(20), log10(20000), 5000);
+    wNorm = 2 * pi * fNorm;
+    [magUnity, ~] = bode(H_total_unity, wNorm);
+    magUnity = squeeze(magUnity);
+    unity_peak = max(magUnity);  % linear peak gain
+    
+    % Apply inverse scalar to all presets so unity peaks at 0 dB (gain = 1; normalized)
+    H_total = H_total * (1 / unity_peak);
+    H_total_unity = H_total_unity * (1 / unity_peak);
+    
+    % Will plot if set to 1
+    if makePlot == 1
+        plotResponse(fc, filters, H_total, H_total_unity, debug);
+    end
+end
---
+++ plotResponse.m
@@ -0,0 +1,86 @@
+%% Helper function: plotResponse
+% Generates plots for a given transfer function + input signal 
+% INPUTS: 
+%   -
+% OUTPUTS:
+%   - 
+
+function [] = plotResponse(fc, filters, H_total, H_total_unity, debug)
+    figure('Name', 'Individual bands + Unity Equalizer');
+    hold on;
+    
+    % Run for each bandpass filter
+    for i = 1:length(fc)
+        bode(filters{i});
+    end
+    
+    % Total system response with unity gain 
+    bode(H_total_unity, '--');
+    
+    title('Frequency Response: Individual Bands and Unity');
+    legend([num2str(fc(1)), ' Hz'], [num2str(fc(2)), ' Hz'], [num2str(fc(3)), ' Hz'], ...
+           [num2str(fc(4)), ' Hz'], [num2str(fc(5)), ' Hz'], [num2str(fc(6)), ' Hz'], ...
+           [num2str(fc(7)), ' Hz'], 'Total System (Unity)')
+    hold off;
+    
+    % FIGURE 2: total summed response w/ provided custom gains
+    
+    figure('Name','Summed Response')
+    hold on;
+
+    % SUMMED EQUALIZER 
+    bode(H_total, 'r');
+    bode(H_total_unity, 'b--');
+    title('Overall Equalizer Response: Custom Gain + Unity')
+    legend('Custom Gain', 'Unity')
+    hold off;
+    
+    if debug == 1
+        % Extract data points for analysis
+        f = logspace(log10(20), log10(20000), 2000);
+        w = 2*pi*f;
+        [mag, ~] = bode(H_total, w);
+        magdB = round(20*log10(squeeze(mag)), 2);
+        % Audible human range: 20 Hz to 20 kHz
+        idx = (f >= 20) & (f <= 20000);
+        magAudible = magdB(idx);
+        fAudible = f(idx);
+        % Min
+        [magMin, idxMin] = min(magAudible);
+        hzMin = fAudible(idxMin);
+        % Max
+        [magMax, idxMax] = max(magAudible);
+        hzMax = fAudible(idxMax);
+        % Peak-to-peak
+        ptop = peak2peak(magAudible);
+        disp(['Min = ', num2str(magMin, '%.2f'), ...
+            ' dB @ ', num2str(hzMin, '%.1f'), ' Hz | ', ...
+            'Max = ', num2str(magMax, '%.2f'), ...
+            ' dB @ ', num2str(hzMax, '%.1f'), ' Hz | ', ...
+            'p-to-p = ', num2str(ptop, '%.2f'), ' dB'])
+    end
+
+    % lsim(b,a,x,t) with x=[1 zeros(1,N)]
+    
+    fs = 44100;   % audio sampling rate 
+    t = 0:(1/fs):0.05; % time vector going from 0 to 0.05 seconds
+    x_imp = [1, zeros(1, length(t)-1)]; % impulse input
+    
+    figure('Name', 'Impulse Responses of All Bands');
+    
+    % Go through each filter, simulate the response
+    for i = 1:length(fc)
+        % get numerator and denominator array of filter 
+        [num_i, den_i] = tfdata(filters{i}, 'v'); % The 'v' param outputs array instead of cell
+        
+        % CT LCCDE response
+        h_i = lsim(num_i, den_i, x_imp, t);
+        
+        % Stacked subplot
+        subplot(length(fc), 1, i);
+        plot(t, h_i);
+        title(['Impulse Response of Band ', num2str(i), ' (', num2str(fc(i)), ' Hz)']);
+        xlabel('Time (seconds)');
+        ylabel('Amplitude');
+    end
+end
